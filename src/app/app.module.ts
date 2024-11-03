d2f25442bf01d883799d87bf46186bd0add6c6c3
nimi17 <nimalraj@codelantic.com>
2024-11-03 22:03:21 +0530

    header-footer-setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './_shared/shared.module';
+import { ComponentsModule } from './layout/components/components.module';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { MainComponent } from './layout/pages/main/main.component';
+import { HeaderComponent } from './layout/pages/header/header.component';
+import { FooterComponent } from './layout/pages/footer/footer.component';
+import { FooterMainComponent } from './layout/pages/footer-main/footer-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    FooterMainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ComponentsModule,
+    NzGridModule,
   ],
   providers: [
     provideClientHydration()
